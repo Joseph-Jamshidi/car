68c2c66a44956e702d1e5f2aa3c7ae29d2c93e31
Yousef Jamshidi <y.jamshidi1990@gmail.com>
2023-09-20 16:34:19 +0330

    car

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
+import HomePage from "./Components/HomePage/HomePage";
+import Header from "./Components/Layout/Header";
+import "./App.css"
 
 const App = () => {
     return (
         <>
             <BrowserRouter>
+                <Header/>
                 <Routes>
                     <Route path="/" element={<HomePage/>}/>
                 </Routes>
